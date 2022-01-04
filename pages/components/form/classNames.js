f6bcda1601f99ee1883531b03510eb892676f3bc
leobtt <leonarbat8@gmail.com>
2022-01-04 20:47:59 -0300

    className

---
+++ pages/components/form/classNames.js
@@ -0,0 +1,4 @@
+export const span = 'w-auto rounded text-red-300 px-3 font-normal'
+export const input =
+  'mb-4 bg-blue-100 border-green-500 focus:shadow-lg w-100 shadow-md rounded-lg py-4 px-5'
+export const label = 'pl-1 mb-1 flex justify-between font-bold'
